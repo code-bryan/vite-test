85caf38ef5d6ff4a5778598d122e015b4a9479a1
code.bryan <brya0196@gmail.com>
2023-03-19 16:35:14 -0400

    feat: include kanvas core

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom'
 import { ApplicationProvider } from '@models/state'
 
 import App from '@pages/app'
+import KanvasCore from '@kanvas/core'
 // import '@theme/style.css'
+
+const client = new KanvasCore({
+  url: '',
+  key: '',
+  middlewares: []
+})
 
 ReactDOM.render(
   <React.StrictMode>
